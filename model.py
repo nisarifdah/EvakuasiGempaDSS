62c11aa5d61dc6372eb2fe4c3e19ea15b072a5ee
Khairunnisa Rifdah <49234459+nisarifdah@users.noreply.github.com>
2021-11-23 23:46:54 +0900

    update search nearest shelter

--- model.py
+++ model.py
@@ -12,8 +12,33 @@ def reverseGeocode(coordinates):
     # result is a list containing ordered dictionary.
     pprint.pprint(result)
 
-def getNearestShelter():
-    print ("McD Dago")
+def dist_between_two_lat_lon(*args):
+    from math import asin, cos, radians, sin, sqrt
+    lat1, lat2, long1, long2 = map(radians, args)
+
+    dist_lats = abs(lat2 - lat1) 
+    dist_longs = abs(long2 - long1) 
+    a = sin(dist_lats/2)**2 + cos(lat1) * cos(lat2) * sin(dist_longs/2)**2
+    c = asin(sqrt(a)) * 2
+    radius_earth = 6378 # the "Earth radius" R varies from 6356.752 km at the poles to 6378.137 km at the equator.
+    return c * radius_earth
+
+
+def findClosestShelter(data, v):
+    try:
+        return min(data, key=lambda p: dist_between_two_lat_lon(v[0],p[0],v[1],p[1]))
+    except TypeError:
+        print('Not a list or not a number.')
+
+
+mcd_dago = [10,45.44]
+kebon_binatang = [50,-9]
+
+shelterList = [mcd_dago, kebon_binatang]
+userLocation = [-6.914744, 107.609810]
+
+
+
 
 def stayAtHome():
     print("Tidak perlu ke Shelter")
@@ -30,7 +55,7 @@ def stayAtHome():
 if location == bandungLocation:
     if skalaGempa > 6:
         if ifTsunami == True:
-            getNearestShelter()
+            print(findClosestShelter(shelterList, userLocation))
         else:
             stayAtHome()
     else:
