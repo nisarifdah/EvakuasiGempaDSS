8af998002a3eb1d636c01f44e6262b09325bda2e
sulistiana18 <43803515+sulistiana18@users.noreply.github.com>
2021-11-26 01:02:10 +0700

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -2,7 +2,16 @@
 #import pprint
 from random import randrange
 import requests
+
+from pywebio.platform.flask import webio_view
+from pywebio import STATIC_PATH
+from flask import Flask, send_from_directory
+from pywebio.input import *
 from pywebio.output import *
+import argparse
+from pywebio import start_server
+
+app = Flask(__name__)
 
 ##bandungLocation = "Yogyakarta"
 bandungLocation = "Bandung"
@@ -103,3 +112,12 @@ if getCity() == bandungLocation:
         put_text("Info Gempa: "+str(skalaGempa)+ " SR\n" + "Tidak Berpotensi Tsunami" + "\n" + "Saran: Tidak perlu ke Shelter!" )
 else:
     put_text("Anda Tidak di Bandung")
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    args = parser.parse_args()
+
+    start_server(getCity, port=args.port)
